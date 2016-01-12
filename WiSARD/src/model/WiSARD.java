65efddf6435b217d649add693df4f4c9fac698b1
Luiz Fernando <luizoliveira.lfro@gmail.com>
2016-01-11 22:46:35 -0200

    Coding WiSARD training and testing methods. Not yet tested.

--- WiSARD/src/model/WiSARD.java
+++ WiSARD/src/model/WiSARD.java
@@ -1,5 +1,133 @@
 package model;
 
-public class WiSARD {
+import java.util.HashMap;
+import java.util.Iterator;
+import java.util.Map.Entry;
 
+public class WiSARD {
+	
+	private int height;
+	private int width;
+	
+	private int tuples;
+	private int rams;
+	
+	private HashMap<String, Discriminator> mapa = new HashMap<String, Discriminator>();
+	private HashMap<Integer, String> relacao1 = new HashMap<Integer, String>();
+	private HashMap<String, Integer> relacao2 = new HashMap<String, Integer>();
+	
+	public WiSARD(int height, int width, int tuples) {
+		
+		this.height = height;
+		this.width = width;
+		this.tuples = tuples;
+		this.rams = height * width / tuples;
+	}
+	
+	public int getTuples() {
+		return this.tuples;
+	}
+	
+	public int getRams() {
+		return this.rams;
+	}
+	
+	public void training(String label, String example ) {
+		
+		long value;
+		long aux;
+		int l;
+		
+		if(mapa.get(label) == null) {
+			
+			Discriminator discriminator = new Discriminator(getRams(), getTuples());
+			discriminator.setId(label);
+			
+			mapa.put(label, discriminator);
+			
+			relacao1.put(relacao1.size(), label);
+			relacao2.put(label, relacao2.size());
+		}
+		
+		l = 0;
+		
+		for (int j = 0; j < mapa.get(label).getTuplas().size(); j = j + getTuples()) {
+			
+			value = 0;
+			aux = (int) Math.pow(2, getTuples());
+			
+			for (int k = j; k < j + getTuples(); k++) {
+				
+				value += Integer.valueOf(example.charAt(mapa.get(label).getTuplas().get(k))) * aux;
+				aux /= 2;
+			}
+			
+			if(mapa.get(label).getRams().get(l).getMapa().get(value) == null)
+			
+				mapa.get(label).getRams().get(l).getMapa().put(value, 1);
+			
+			else
+				
+				mapa.get(label).getRams().get(l).getMapa().put(value, mapa.get(label).getRams().get(l).getMapa().get(value) + 1);
+			
+			l++;
+		}
+	}
+	
+	public String testing(String test) {
+		
+		String label = "";
+		
+		long value;
+		long aux;
+		int l;
+		
+		int[] similarity = new int[mapa.size()];
+		
+		Iterator<Entry<String, Discriminator>> iterador = mapa.entrySet().iterator();
+		
+		while(iterador.hasNext()) {
+			
+			Entry<String, Discriminator> elemento = iterador.next();
+			
+			l = 0;
+			
+			for (int j = 0; j < elemento.getValue().getTuplas().size(); j = j + getTuples()) {
+				
+				value = 0;
+				aux = (int) Math.pow(2, getTuples());
+				
+				for (int k = j; k < j + getTuples(); k++) {
+					
+					value += Integer.valueOf(test.charAt(mapa.get(elemento.getKey()).getTuplas().get(k))) * aux;
+					aux /= 2;
+				}
+				
+				if(mapa.get(elemento.getKey()).getRams().get(l).getMapa().get(value) != null)
+				
+					similarity[relacao2.get(elemento.getKey())]++;
+				
+				l++;
+			}
+		}
+		
+		int maxIndex = 0;
+		
+		for (int j = 1; j < similarity.length; j++) {
+			
+			
+			if(similarity[j] > similarity[maxIndex]) {
+				
+				maxIndex = j;
+			}
+		}
+		
+		label = relacao1.get(maxIndex);
+		
+		return label;
+	}
+	
+	public void mentalImage() {
+		
+	}
 }
