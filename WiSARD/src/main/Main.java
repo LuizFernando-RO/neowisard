696db011be46b3468dea577c7b0aca2b5d78220c
Luiz Fernando <luizoliveira.lfro@gmail.com>
2016-01-13 18:28:26 -0200

    Created methods for splitting the dataset into different forms.

--- WiSARD/src/main/Main.java
+++ WiSARD/src/main/Main.java
@@ -10,6 +10,7 @@ import java.io.IOException;
 import java.util.ArrayList;
 import java.util.HashMap;
 import java.util.Iterator;
+import java.util.Random;
 import java.util.Map.Entry;
 import java.util.concurrent.TimeUnit;
 
@@ -25,16 +26,21 @@ public class Main {
 		startTime = System.nanoTime();
 		
 		WiSARD w1 = new WiSARD("w1", 28,28,28);
+		WiSARD w2 = new WiSARD("w1", 28,28,28);
+		
+		totalSplitFiles();
 		
 		//splitFiles();
 		
-		train(w1, 60000, "Input/MNIST/Original/training.csv");
+		//train(w1, 60000, "Input/MNIST/Original/training.csv");
 		
 		//System.out.println(w1.toString());
 		
 		//test(w1, 10000, "Input/MNIST/Original/testing.csv");
 		
 		//w1.mentalImage("5");
+		
+		//randomSplitFiles();
 		
 		endTime = System.nanoTime();
 		
@@ -162,7 +168,141 @@ public class Main {
 		System.out.println("\n-- Testing phase finished successfully --");
 	}
 	
-	public static void splitFiles() {
+	// This method divide the dataset into 2 files
+	public static void autoSplitFiles() {
+		
+		int trainingSize = 60000;
+		
+		File f = new File("Input/MNIST/Original/training.csv");
+		File f2 = new File("Input/MNIST/Auto Split/train1.txt");
+		
+		FileReader fr;
+		FileWriter fw;
+		
+		try {
+			
+			fr = new FileReader(f);
+			fw = new FileWriter(f2);
+			
+			BufferedReader br = new BufferedReader(fr);
+			BufferedWriter bw = new BufferedWriter(fw);
+			
+			for(int i = 0; i < trainingSize / 2; i++) 
+			
+				bw.write(br.readLine().trim() + "\n");
+			
+			bw.close();
+			
+			f2 = new File("Input/MNIST/Auto Split/train2.txt");
+			
+			fw = new FileWriter(f2);
+			bw = new BufferedWriter(fw);
+			
+			for(int i = trainingSize / 2; i < trainingSize; i++) 
+				
+				bw.write(br.readLine().trim() + "\n");
+			
+			bw.close();
+			br.close();
+			
+		} catch (FileNotFoundException e) {
+			
+			System.out.println("File not found!");
+		} catch (IOException e) {
+			
+			System.out.println("Error!");
+		}
+	}
+	
+	// This method divide the dataset into 2 files with random samples
+	public static void randomSplitFiles() {
+		
+		int trainingSize = 60000;
+		
+		ArrayList<String> arr = new ArrayList<String>();
+		
+		int[] array = new int[trainingSize];
+		int posicao, aux;
+		
+		Random rand = new Random();
+
+		for (int i = 0; i < array.length; i++) {
+			
+			array[i] = i;	
+		}
+		
+		for (int i = 0; i < array.length; i++) {
+			
+			posicao = rand.nextInt(trainingSize);
+			
+			aux = array[i];
+			array[i] = array[posicao];
+			array[posicao] = aux;
+		}
+		
+		File f = new File("Input/MNIST/Original/training.csv");
+		
+		FileReader fr;
+		BufferedReader br;
+		
+		try {
+			
+			fr = new FileReader(f);
+			br = new BufferedReader(fr);
+			
+			for(int i = 0; i < trainingSize; i++)
+				
+				arr.add(br.readLine().trim());
+			
+			br.close();
+			
+		} catch (FileNotFoundException e) {
+			
+			System.out.println("File not found!");
+		} catch (IOException e) {
+			
+			System.out.println("Error!");
+		}
+	
+		File f2 = new File("Input/MNIST/Random Split/train1.txt");
+		
+		FileWriter fw;
+		BufferedWriter bw;
+		
+		try {
+			
+			fw = new FileWriter(f2);
+			bw = new BufferedWriter(fw);
+			
+			for(int i = 0; i < trainingSize / 2; i++)
+				
+				bw.write(arr.get(array[i]) + "\n");
+			
+			bw.close();
+			
+			f2 = new File("Input/MNIST/Random Split/train2.txt");
+
+			fw = new FileWriter(f2);
+			bw = new BufferedWriter(fw);
+			
+			for(int i = trainingSize / 2; i < trainingSize; i++)
+				
+				bw.write(arr.get(array[i]) + "\n");
+			
+			bw.close();
+
+			
+		} catch (FileNotFoundException e) {
+			
+			System.out.println("File not found!");
+		} catch (IOException e) {
+			
+			System.out.println("Error!");
+		}
+	}
+	
+	// This method divide the dataset into 10 files, according to the sample label
+	public static void totalSplitFiles() {
 		
 		int trainingSize = 60000;
 		
@@ -220,14 +360,19 @@ public class Main {
 			
 			Entry<String, ArrayList<String>> element = iterator.next();
 			
-			f = new File("Input/MNIST/Divided/" + element.getKey() + ".txt");
+			f = new File("Input/MNIST/Total Split/" + element.getKey() + ".txt");
 			
 			FileWriter fw;
+			BufferedWriter bw;
 			
 			try {
 				
+				if(!f.exists())
+					
+					f.createNewFile();
+				
 				fw = new FileWriter(f);
-				BufferedWriter bw = new BufferedWriter(fw);
+				bw = new BufferedWriter(fw);
 				
 				for(int i = 0; i < element.getValue().size(); i++) {
 					
